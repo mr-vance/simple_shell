35911df211a162557ca85c79e78fb3ed6f563963
Vance <muchongo.vance@gmail.com>
2023-11-17 11:42:29 +0000

    testing

--- _atoi.c
+++ _atoi.c
@@ -8,7 +8,8 @@
  */
 int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	struct stat stat_buf;
+    return (isatty(STDIN_FILENO) && fstat(info->readfd, &stat_buf) != -1);
 }
 
 /**
@@ -19,10 +20,11 @@ int interactive(info_t *info)
  */
 int is_delim(char c, char *delim)
 {
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
+    while (*delim != '\0') {
+        if (c == *delim++)
+            return 1;
+    }
+    return 0;
 }
 
 /**
@@ -33,10 +35,7 @@ int is_delim(char c, char *delim)
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+	return isalpha(c);
 }
 
 /**
@@ -47,28 +46,5 @@ int _isalpha(int c)
 
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
+	return atoi(s);
 }
